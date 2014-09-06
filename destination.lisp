691d7ec695f480bea15af39b3342a5622dd84016
isoraqathedh <isoraqathedh.zorethan@gmail.com>
2014-09-06 15:19:21 +0800

    More formatting.

--- destination.lisp
+++ destination.lisp
@@ -23,11 +23,11 @@
   (:documentation "A representation of what a piece can do at a square."))
 
 (defmethod print-object ((object destination) stream)
-  (let ((*print-level* 4)
+  (let ((*print-level* 3)
         (*print-pretty* nil))
     (print-unreadable-object (object stream :type t)
       (with-accessors ((x destination-x) (y destination-y) (signature signature)) object
-        (format stream "(~a, ~a) ~s" x y signature)))))
+        (format stream "(~2d, ~2d) ~{~s~^ ~}" x y signature)))))
 
 (defclass compound-destination ()
   ((sequence :accessor elements
