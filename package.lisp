3dfeda9689a74f684cd391ed67c1559473435143
Isoraķatheð Zorethan <isoraqathedh.zorethan@gmail.com>
2018-12-10 16:53:15 +0800

    Merging commits.

--- package.lisp
+++ package.lisp
@@ -1,20 +1,27 @@
-(defpackage #:info.isoraqathedh.betza-lexer
-  (:use #:cl #:plump)
-  (:nicknames :betza-lexer)
-  (:export 
-   #:power
-   #:landmark
-   #:modifiers
-   #:limit
-   #:piece
-   #:powers
-   #:parse-piece
-   #:string->power))
+;;; package.lisp
 
-(defpackage #:info.isoraqathedh.betza-board
-  (:use #:cl #:iterate)
-  (:nicknames :betza-board))
-
+;;; also handles readtable definition
 (defpackage #:info.isoraqathedh.betza
-  (:use #:cl #:betza-lexer #:betza-board)
-  (:nicknames :betza))
+  (:use #:cl)
+  (:export #:betza-readtable))
+
+(in-package #:info.isoraqathedh.betza)
+(named-readtables:defreadtable betza-readtable
+  (:merge nil))
+
+
+;; (defpackage #:info.isoraqathedh.betza-lexer
+;;   (:use #:cl #:plump #:named-readtables)
+;;   (:nicknames :betza-lexer)
+;;   (:export
+;;    #:power
+;;    #:landmark
+;;    #:modifiers
+;;    #:limit
+;;    #:piece
+;;    #:powers
+;;    #:parse-piece
+;;    #:string->power))
+
+(defpackage #:info.isoraqathedh.betza.generators
+  (:use :cl))
